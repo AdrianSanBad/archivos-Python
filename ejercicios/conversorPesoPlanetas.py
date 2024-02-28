edad1c4486818381385f91c88435295352ccc4b5
adriansb <adriansb2702@gmail.com>
2024-02-28 01:38:01 -0600

    Nuevo ejercicio agregado

---
+++ ejercicios/conversorPesoPlanetas.py
@@ -0,0 +1,28 @@
+# Write code below 💖
+relativeG=[0.38,0.91,0.38,2.53,1.07,0.89,1.14]
+earthW=float(input("Weigth in Earth? "))
+planetN=int(input("Planet number? "))
+if planetN==1:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==2:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==3:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==4:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==5:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==6:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+elif planetN==7:
+  destinationWeight=earthW*relativeG[planetN-1]
+  print(destinationWeight)
+else:
+  print("Invalid planet number")
+
