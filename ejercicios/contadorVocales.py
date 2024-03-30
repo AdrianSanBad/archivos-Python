9c542d198eb8d9d12560ca6025a6bbc3fe0680fe
adriansb <adriansb2702@gmail.com>
2024-03-29 21:50:36 -0600

    Contador vocales

--- ejercicios/contadorVocales.py
+++ ejercicios/contadorVocales.py
@@ -2,9 +2,9 @@
 en esa entrada. Las vocales son las letras 'a', 'e', 'i', 'o', 'u' (tanto en minúsculas como en mayúsculas)."""
 
 frase = input("Ingrese una frase o párrafo: ")
-vocales = "aeiouAEIOU"  
+vocales = "aeiouAEIOU"  # Cadena de vocales
 contador = 0
-for letra in frase:
-    if letra in vocales:
-        contador += 1
+for letra in frase: # Itera sobre cada letra de la frase
+    if letra in vocales: # Si la letra está en la cadena de vocales
+        contador += 1 # Aumenta el contador
 print(f"El número de vocales en la frase o párrafo es: {contador}")
