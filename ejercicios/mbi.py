5459bc68ae7b445e15b0780caf170ca25cb56890
adriansb <adriansb2702@gmail.com>
2024-04-04 20:35:33 -0600

    cambios solicitados indice de masa corpotar

--- ejercicios/mbi.py
+++ ejercicios/mbi.py
@@ -1,5 +1,20 @@
 #Programa que calcula el indice de masa corporal solicitando el peso en kilogramos y la altura en metros
-mass=int(input("peso en kilogramos: ")) #input solicita el peso en kilogramos
-heigth=float(input("Altura en metros: ")) #input solicita la altura en metros
-bmi=(mass)/(heigth**2) #formula para calcular el indice de masa corporal
-print(f"{bmi} es tu indice de masa corporal") 
+def mbi():
+    mass=int(input("peso en kilogramos: ")) #input solicita el peso en kilogramos
+    heigth=float(input("Altura en metros: ")) #input solicita la altura en metros
+    indice=(mass)/(heigth**2) #formula para calcular el indice de masa corporal
+    print(f"{indice} es tu indice de masa corporal") 
+    #Se determina el estado de la persona segun el indice de masa corporal
+    if indice<18.5:
+        print("Bajo peso")
+    elif indice>=18.5 and indice<24.9:
+        print("Peso normal")
+    elif indice>=25 and indice<29.9:
+        print("Sobrepeso")
+    elif indice>=30 and indice<34.9:
+        print("Obesidad tipo I")
+    elif indice>=35 and indice<39.9:
+        print("Obesidad tipo II")
+    else:
+        print("Obesidad tipo III (mórbida)")
+mbi()
