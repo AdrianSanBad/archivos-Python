dea52c649215eae2c7a6f68da63b3119ebfdb475
adriansb <adriansb2702@gmail.com>
2024-04-17 22:54:48 -0600

    ejercicio 2 codedex python intermedio

---
+++ Python Intermedio/02FindMyFriends.py
@@ -0,0 +1,9 @@
+city=("Pachuca",)
+#3 amigos
+mike=("20.608500","-100.401960")
+yaz=("20.122230","-98.721860")
+jose=("20.116730","-98.714560")
+print(mike)
+print(yaz)
+print(jose)
+#miguel es el mas lejano
