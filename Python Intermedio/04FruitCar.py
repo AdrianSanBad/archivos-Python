f24155f721098c9d28f32067e82fd168c084ed38
adriansb <adriansb2702@gmail.com>
2024-04-19 23:28:10 -0600

    ejercicio 4 codedex

---
+++ Python Intermedio/04FruitCar.py
@@ -0,0 +1,9 @@
+#creando SETS para saber su funcionamiento y estructura
+myFruits={"platano","manzana","mango","piña"}
+friendFruit={"platano","pera","uvas","piña"}
+unidos=myFruits.union(friendFruit)
+inter=myFruits.intersection(friendFruit)
+diferencia=myFruits.difference(friendFruit)
+print(unidos)
+print(inter)
+print(diferencia)
