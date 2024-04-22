924fccf39ff941c583c2f2e1b19676f8cf30a854
adriansb <adriansb2702@gmail.com>
2024-04-21 21:56:53 -0600

    ejercicio 6 codedex

---
+++ Python Intermedio/06DearDairy.py
@@ -0,0 +1,5 @@
+#cree un archivo de texto llamado diaries.txt y escribi en el tres lineas de texto que describieron mi dia
+file=open("diaries.txt", "w")
+file.write("El dia de hoy fui al cine, vi la pelicula de godzilla \n")
+file.write("despues de haber visto esa pelicula fui con mis primos a su casa \n")
+file.write("despues regrese a mi casa y ahora hago tarea, deseo comprarme un carro")
