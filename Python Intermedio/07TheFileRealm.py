670d552a0b48b7a30f7d422d37457c529166df75
adriansb <adriansb2702@gmail.com>
2024-04-25 14:13:24 -0600

    ejercicio 7 de codedex uso de diccionario en modificacion de textos

---
+++ Python Intermedio/07TheFileRealm.py
@@ -0,0 +1,13 @@
+#uso de diccionarios para escribir en un archivo de texto las canciones que me gustan
+liked_songs={
+  "From the start": "Laufey",
+  "Feel it": "D4vd",
+  "ARE WE STILL FRIENDS?": "Tyler, The Creator"
+}
+def write_liked_songs_to_file(liked_songs,file_name):
+  documento=open(file_name,"w")
+  documento.write("Canciones que me gustan: \n")
+  for titulo,artista in liked_songs.items():
+    documento.write(f"{titulo} by {artista} \n")
+
+write_liked_songs_to_file(liked_songs, "canciones.txt")
